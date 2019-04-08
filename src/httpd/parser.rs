5b691a9410347928338193bb52443987ac13527f
Paul Brinkmeier <paul.brinkmeier@gmail.com>
2019-04-08 14:49:56 +0200

    Add HTTP header parsing

--- src/httpd/parser.rs
+++ src/httpd/parser.rs
@@ -1,5 +1,6 @@
 use alloc::{
     borrow::ToOwned,
+    collections::BTreeMap,
     string::{String, ToString},
 };
 
@@ -10,6 +11,7 @@ pub struct HTTPParser {
     method: Option<String>,
     path: Option<String>,
     version: Option<String>,
+    headers: BTreeMap<String, String>,
 }
 
 #[derive(Debug)]
@@ -25,6 +27,7 @@ impl HTTPParser {
             method: None,
             path: None,
             version: None,
+            headers: BTreeMap::new(),
         }
     }
 
@@ -35,11 +38,13 @@ impl HTTPParser {
             .and_then(|_| self.expect(" "))
             .and_then(|_| self.parse_version())
             .and_then(|_| self.expect("\r\n"))
+            .and_then(|_| self.parse_headers())
             .and_then(|_| {
                 Ok(Request::new(
                     self.method.to_owned().unwrap(),
                     self.path.to_owned().unwrap(),
                     self.version.to_owned().unwrap(),
+                    self.headers.to_owned()
                 ))
             })
     }
@@ -80,6 +85,34 @@ impl HTTPParser {
             self.version = Some(version.to_owned());
             version
         })
+    }
+
+    fn parse_headers(&mut self) -> Result<(), ParseError> {
+        loop {
+            if self.source.starts_with("\r\n") {
+                return Ok(());
+            }
+
+            match self.parse_header() {
+                Ok((key, value)) => {
+                    self.headers.insert(key, value);
+                }
+                Err(e) => return Err(e),
+            }
+        }
+    }
+
+    fn parse_header(&mut self) -> Result<(String, String), ParseError> {
+        let mut maybe_key = None;
+        let mut maybe_value = None;
+
+        self.read_until(":")
+            .map(|key| maybe_key = Some(key))
+            .and_then(|_| self.expect(":"))
+            .and_then(|_| self.read_until("\r\n"))
+            .map(|value| maybe_value = Some(value))
+            .and_then(|_| self.expect("\r\n"))
+            .map(|_| (maybe_key.unwrap(), maybe_value.unwrap().trim().to_string()))
     }
 
     fn expect(&mut self, expected: &str) -> Result<String, ParseError> {
--- src/httpd/request.rs
+++ src/httpd/request.rs
@@ -1,18 +1,23 @@
-use alloc::string::String;
+use alloc::{
+    collections::BTreeMap,
+    string::{String, ToString}
+};
 
 #[derive(Debug)]
 pub struct Request {
     method: String,
     path: String,
     version: String,
+    headers: BTreeMap<String, String>
 }
 
 impl Request {
-    pub fn new(method: String, path: String, version: String) -> Request {
+    pub fn new(method: String, path: String, version: String, headers: BTreeMap<String, String>) -> Request {
         Request {
             method,
             path,
             version,
+            headers,
         }
     }
 
@@ -26,5 +31,9 @@ impl Request {
 
     pub fn version(&self) -> &str {
         &self.version
+    }
+
+    pub fn get_header(&self, key: String) -> Option<String> {
+        self.headers.get(&key).map(|maybe_val| maybe_val.to_string())
     }
 }
