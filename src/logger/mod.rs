3bb744fd22cae1ab37f60c6712ed341f860c900d
Lukas Brocke <lukas@brocke.net>
2019-04-10 16:33:03 +0200

    logger: Indent messages with more than one line

--- src/logger/mod.rs
+++ src/logger/mod.rs
@@ -26,11 +26,11 @@
 //!     logger::init().unwrap();
 //!
 //!     // prints something like
-//!     //   "[   15.325] This is an informational message"
+//!     //   "[  15.32] This is an informational message"
 //!     info!("This is an informational message");
 //!
 //!     // prints something like
-//!     //   "[   15.331] (Warn) This is a warning"
+//!     //   "[  15.33] (Warn) This is a warning"
 //!     warn!("This is a warning");
 //! }
 //! ```
@@ -38,12 +38,15 @@
 extern crate log;
 
 use alloc::string::String;
+use alloc::vec::Vec;
 use log::{Level, LevelFilter, Metadata, Record, SetLoggerError};
 use stm32f7_discovery::{lcd, print, println, system_clock};
 
 struct Stm32f7Logger;
 
 static LOGGER: Stm32f7Logger = Stm32f7Logger;
+
+const NUM_COLS: usize = 60;
 
 impl log::Log for Stm32f7Logger {
     // This logger implements all levels, therefore always return true
@@ -52,27 +55,60 @@ impl log::Log for Stm32f7Logger {
     }
 
     fn log(&self, record: &Record) {
-        if !lcd::stdout::is_initialized() {
+        if !lcd::stdout::is_initialized() || !self.enabled(record.metadata()) {
             return;
         }
 
-        if self.enabled(record.metadata()) {
-            let clock = system_clock::ms() as u64;
-            let sec = clock / 1000;
-            let ms = clock - sec * 1000;
+        let clock = system_clock::ms() as u64;
+        let sec = clock / 1000;
+        let ms = (clock - sec * 1000) / 10;
 
-            // print log level for every level except Info
-            let lvl = match record.level() {
-                Level::Info => String::from(""),
-                _ => format!("({:?}) ", record.level()),
-            };
+        // see module description above for format explanation
+        let prefix = format!("[{:>4}.{:>02}] ", sec, ms);
 
-            // see module description above for format explanation
-            println!("[{:>4}.{:>03}] {}{}", sec, ms, lvl, record.args());
+        // print log level for every level except Info
+        let lvl = match record.level() {
+            Level::Info => String::from(""),
+            _ => format!("({:?}) ", record.level()),
+        };
+
+        let message = format!("{}{}", lvl, record.args());
+
+        // split message into fixed-sized substrings
+        let prefix_len = prefix.chars().count();
+        let lines = split_string(&message, NUM_COLS - prefix_len);
+
+        for line in 0..lines.len() {
+            if line == 0 {
+                print!("{}", prefix);
+            } else {
+                // indent following lines with whitespace
+                print!("{}", " ".repeat(prefix_len));
+            }
+
+            println!("{}", lines[line]);
         }
     }
 
     fn flush(&self) {}
+}
+
+// splits a string into fixed-size substrings
+fn split_string(string: &str, sub_len: usize) -> Vec<&str> {
+    let mut subs = Vec::with_capacity(string.len() / sub_len);
+    let mut iter = string.chars();
+    let mut pos = 0;
+
+    while pos < string.len() {
+        let mut len = 0;
+        for ch in iter.by_ref().take(sub_len) {
+            len += ch.len_utf8();
+        }
+        subs.push(&string[pos..pos + len]);
+        pos += len;
+    }
+
+    subs
 }
 
 // initialize logger and enable all logging levels
